fb454683c9e302b63502b2ba12c83f3b58ce3512
Adam Carter <adm.carter@gmail.com>
2020-10-08 01:41:30 -0400

    Update environment variable names

--- twitoff/__init__.py
+++ twitoff/__init__.py
@@ -2,11 +2,14 @@ import os
 from os import getenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 # basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABAS_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URL")
 # 'sqlite:///' + os.path.join(basedir, 'twitoff.db')
 app.config['SQL_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'mysecretkey')
